c15f0fd724f527ab7dfcd22881011951fa7995db
arundhaj <arundhaj@gmail.com>
2012-04-26 11:40:57 +0530

    sample to test control flow

---
+++ userinput/numcondition.py
@@ -0,0 +1,12 @@
+# a simple program to test the control flow
+
+num = int(input('Enter a number: '))
+
+if num == 0:
+	print('Equal to 0')
+elif num < 0:
+	print('Negative number')
+else: 
+	print('Positive number')
+
+print ('Done')
